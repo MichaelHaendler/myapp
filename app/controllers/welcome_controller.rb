2637dcfc64db75a2d0b2e35d289c99d40e617a54
Michael Haendler <Michael.Haendler@gmail.com>
2015-04-19 19:21:19 -0400

    trying again with a call to Book table

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
 
 	def zoomy()
-		#@zoom = Book.find(3).inspect
-		@zoom = "blah"
+		@zoom = Book.find(3).inspect
+
+		#the below zoom worked. I was able to refer to it in the view for https://evening-dawn-1086.herokuapp.com/
+		#@zoom = "blah" 
 	end
 
 end
