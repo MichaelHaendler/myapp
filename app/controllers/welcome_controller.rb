bf50ac407cf6d89297ed36f6d216495e4183e058
Michael Haendler <Michael.Haendler@gmail.com>
2015-04-20 18:39:12 -0400

    probably wont let me upload however I know that in this version at least the psql stuff is all working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def zoomy()
-		@zoom = Book.find(3).inspect
+		@zoom = Book.first.title
 
 		#the below zoom worked. I was able to refer to it in the view for https://evening-dawn-1086.herokuapp.com/
 		#@zoom = "blah" 
