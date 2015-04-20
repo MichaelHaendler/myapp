f9e9623d9eaefb3ebd800e09eb206f6428c7fadc
Michael Haendler <Michael.Haendler@gmail.com>
2015-04-20 19:50:53 -0400

    now book automatically has something in it

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
 	def zoomy()
+
+		Book.create(title: "testing")
+
 		@zoom = Book.first.title
 
 		#the below zoom worked. I was able to refer to it in the view for https://evening-dawn-1086.herokuapp.com/
